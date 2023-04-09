f2571c8877a575da3994b973f71f55bb79353a5e
wahidgoni17 <imwahidgoni17@gmail.com>
2023-04-10 02:39:55 +0600

    show all button added

--- src/components/Featured/Feature.jsx
+++ src/components/Featured/Feature.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faDollarSign } from "@fortawesome/free-solid-svg-icons";
 
 const Feature = ({ feature }) => {
   const {
@@ -12,20 +14,30 @@ const Feature = ({ feature }) => {
   } = feature;
   console.log(feature);
   return (
-    <div className="p-6 shadow-xl px-auto">
+    <div className="shadow-xl py-12 px-auto">
       <img className="w-40 h-32 mx-auto my-4" src={company_logo} alt="" />
       <div className="text-center">
-        <p>{job_title}</p>
-        <p>{company_name}</p>
-        <div className="flex justify-center gap-5">
-          <p>{remote_or_onsite}</p>
-          <p>{fulltime_or_parttime}</p>
+        <p className="text-xl font-medium">{company_name}</p>
+        <p className="text-3xl font-semibold">{job_title}</p>
+        <div className="flex justify-center items-center py-3 gap-5">
+          <p className="border border-indigo-300 rounded-md text-indigo-600  p-2">
+            {remote_or_onsite}
+          </p>
+          <p className="border border-indigo-300  rounded-md text-indigo-600 p-2">
+            {fulltime_or_parttime}
+          </p>
         </div>
         <div className="flex justify-center gap-5">
-          <p>{location}</p>
-          <p>{salary}</p>
+          <p>
+            <FontAwesomeIcon className="mr-2" icon={faLocationDot} />
+            {location}
+          </p>
+          <p>
+            <FontAwesomeIcon className="mr-2" icon={faDollarSign} />
+            Salary: {salary}
+          </p>
         </div>
-        <button className="btn btn-primary my-5">View Details</button>
+        <button className="btn btn-primary mt-4">View Details</button>
       </div>
     </div>
   );
